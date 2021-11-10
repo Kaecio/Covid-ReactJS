4e77d599679e57334c3d29671135edad7dd4e3b5
Kaecio <kaecio@yahoo.com.br>
2021-11-10 12:57:19 -0300

    Add ALT in IMG

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import a from './img/heart.ico'
 function Footer() {
   return (
     <div className="footer">
-      <p className="author">Developed by Fanderson Kaécio. All rights reserved.<img src={a} /></p>
+      <p className="author">Developed by Fanderson Kaécio. All rights reserved.<img src={a} alt="green-heart"/></p>
       
     </div>
   )
